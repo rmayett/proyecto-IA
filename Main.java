5a98f08c38a96afc8346ba9cefad705757a9f929
rmayett <rmayett@hotmail.com>
2018-11-29 12:47:07 -0600

    impresion de la primera generacion

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.io.*;
 public class Main {
     public void poblacion(){
+        File f = new File("Generaciones.txt");
+        try{
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw);
+        pw.println("Primera Generacion");
         int i=0,j=0;
         Persona[] per=new Persona[50];
         do{
@@ -10,10 +17,22 @@ public class Main {
                     i++;
                 }while(i!=per[j].persona.length);
             System.out.println("No. "+(j+1));
+            pw.println("No. "+(j+1));
+            for (int p =0;p<per[j].getLong();p++) {
+			pw.print(per[j].getPersona(p)+" ");
+		}
+            pw.println("");
             per[j].show();
                 j++;
         }while(j<50);
-        
+        //llamamos a seleccion
+        //llamamos cruza
+        //Mostramos generacion 
+        pw.println("Segunda Generacion");
+        //metemos en condicion de repeticion 
+        pw.close();
+        bw.close();
+        }catch(IOException e){};
 	}
 	public static void main(String[] args) {
 		Main proyecto = new Main();
--- Persona.java
+++ Persona.java
@@ -3,16 +3,22 @@ public class Persona {
     Random r = new Random();
     public int persona[]; 
 	public Persona(){
-		this.persona = new int[10];
+		this.persona = new int[16];
 	}
+        public int getLong(){
+            return this.persona.length;
+        }
+        public int getPersona(int i){
+           return this.persona[i];
+        }
 	public void show(){
             System.out.println("Persona:");
 		for (int i =0;i<this.persona.length;i++) {
-			System.out.print(" "+this.persona[i]);			
+			System.out.print(" "+this.persona[i]);
 		}
             System.out.println("");
 	}
 	public void agrega(int i){
-            this.persona[i]=r.nextInt(10);         		
+            this.persona[i]=r.nextInt(16)+1;         		
 	}   
 }
